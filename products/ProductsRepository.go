c1fe4a311a5e042c28655a1859d03ec305392836
djm <djordje.mijatovic.djole@gmail.com>
2021-09-12 11:40:23 +0200

    v0.4 Fetch products from database

--- products/ProductsRepository.go
+++ products/ProductsRepository.go
@@ -1,82 +1,82 @@
 package products
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
+    "database/sql"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+    "log"
 )
 
 type Repository struct {
-	Database *sql.DB
+    Database *sql.DB
 }
 
 var mysqlDatabase = Repository{
-	Database: connectDb(),
+    Database: connectDb(),
 }
 
 func connectDb() *sql.DB {
-	database, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/schema_MopShop")
+    database, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/schema_MopShop")
 
-	if err != nil {
-		log.Fatalf("Cannot conntect to database. Terminated.")
-	}
+    if err != nil {
+        log.Fatalf("Cannot conntect to database. Terminated.")
+    }
 
-	return database
+    return database
 }
 
 func getAllProducts() ([]Product, error) {
-	result, err := mysqlDatabase.Database.Query("" +
-		"SELECT * " +
-		"FROM products")
-	defer result.Close()
+    result, err := mysqlDatabase.Database.Query("" +
+        "SELECT * " +
+        "FROM products")
+    defer result.Close()
 
-	if err != nil {
-		log.Printf("Cannot read data from database.")
+    if err != nil {
+        log.Printf("Cannot read data from database.")
 
-		return []Product{}, err
-	}
+        return []Product{}, err
+    }
 
-	var products []Product
-	for result.Next() {
-		var product Product
-		err = result.Scan(&product.Id, &product.Name, &product.ImageUrl, &product.Description, &product.Price)
+    var products []Product
+    for result.Next() {
+        var product Product
+        err = result.Scan(&product.Id, &product.Name, &product.ImageUrl, &product.Description, &product.Price)
 
-		if err != nil {
-			log.Printf("Exception %s ", err.Error())
+        if err != nil {
+            log.Printf("Exception %s ", err.Error())
 
-			return []Product{}, err
-		}
+            return []Product{}, err
+        }
 
-		products = append(products, product)
-	}
+        products = append(products, product)
+    }
 
-	return products, nil
+    return products, nil
 }
 
 func getProduct(productId int) (Product, error) {
-	result, err := mysqlDatabase.Database.Query(""+
-		"SELECT * "+
-		"FROM products "+
-		"WHERE id = ?", productId)
-	defer result.Close()
+    result, err := mysqlDatabase.Database.Query(""+
+        "SELECT * "+
+        "FROM products "+
+        "WHERE id = ?", productId)
+    defer result.Close()
 
-	if err != nil {
+    if err != nil {
 
-	}
+    }
 
-	if result.Next() {
-		var product Product
-		err = result.Scan(&product.Id, &product.Name, &product.ImageUrl, &product.Description, &product.Price)
+    if result.Next() {
+        var product Product
+        err = result.Scan(&product.Id, &product.Name, &product.ImageUrl, &product.Description, &product.Price)
 
-		if err != nil {
-			log.Printf("Exception %s ", err.Error())
+        if err != nil {
+            log.Printf("Exception %s ", err.Error())
 
-			return Product{}, err
-		}
+            return Product{}, err
+        }
 
-		return product, nil
-	}
+        return product, nil
+    }
 
-	return Product{}, fmt.Errorf("Cannot find a product with id %d.", productId)
+    return Product{}, fmt.Errorf("Cannot find a product with id %d.", productId)
 }
